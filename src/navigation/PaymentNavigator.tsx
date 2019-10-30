a57fd6f681fa7b84ce83174103e82dc6a296bc55
Ryan Nesbitt <ryannesb@gmail.com>
2019-10-30 12:29:25 -0400

    Changes

--- src/navigation/PaymentNavigator.tsx
+++ src/navigation/PaymentNavigator.tsx
@@ -1,13 +1,16 @@
 import { createStackNavigator } from 'react-navigation-stack'
 
 import HomeScreen from '../screens/payment/HomeScreen'
+import NewPaymentScreen from '../screens/payment/NewPaymentScreen'
 
 export const PaymentNavigator = createStackNavigator(
     {
         Home: HomeScreen,
+        New: NewPaymentScreen
     },
     { 
-        initialRouteName: 'Home'
+        initialRouteName: 'Home',
+        headerMode: 'none',
     }
 )
 
