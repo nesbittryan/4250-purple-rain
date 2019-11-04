a6be70302d20d951489cd30e07b0665b2896e905
Adam Sharanewych <asharane@uoguelph.ca>
2019-11-04 16:18:26 -0500

    WIP messages to DB

---
+++ src/service/ChatService.tsx
@@ -0,0 +1,73 @@
+import firebase from 'firebase'
+import { thisExpression } from '@babel/types'
+
+class ChatService {
+  uid = ''
+  username = ''
+  messagesRef = null
+
+  constructor() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyArS9sNG2L12Z9-nxitqzXFiFP3lruWyug",
+      authDomain: "purplerain-1513a.firebaseapp.com",
+      databaseURL: "https://purplerain-1513a.firebaseio.com",
+      projectId: "purplerain-1513a",
+      storageBucket: "purplerain-1513a.appspot.com",
+      messagingSenderId: "529070849960",
+      appId: "1:529070849960:web:afebcd864096d73573679d",
+      measurementId: "G-DJV3SFR0ND"
+    })
+  }
+
+  setUid(value) {
+    this.uid = value
+  }
+
+  getUid() {
+    return this.uid
+  }
+
+  setUserName(value) {
+    this.username = value
+  }
+
+  getUserName() {
+    return this.username
+  }
+
+  loadMessages(callback) {
+    this.messagesRef = firebase.database().ref('messages')
+    this.messagesRef.off()
+    const onReceive = (data) => {
+      const message = data.val()
+      callback({
+        _id: data.key,
+        text: message.text,
+        createdAt: new Date(message.createdAt),
+        user : {
+          _id: message.user._id,
+          name: message.user.name,
+        },
+      })
+    }
+    this.messagesRef.limitToLast(20).on('child_added', onReceive)
+  }
+
+  sendMessage(message) {
+    for (let i = 0; i < message.length; i++) {
+      this.messagesRef.push({
+        text: message[i].text,
+        user: message[i].user,
+        createdAt: firebase.database.ServerValue.TIMESTAMP,
+      })
+    }
+  }
+
+  closeChat() {
+    if (this.messagesRef) {
+      this.messagesRef.off();
+    }
+  }
+}
+
+export default new ChatService();
